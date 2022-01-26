e0339f032d498c5a6d08e24eb20fe25bac50e218
danishm4557 <danishm4557@gmail.com>
2022-01-26 16:10:09 -0600

    set up complete

---
+++ chew-front-end/src/components/RestaurantAddress.js
@@ -0,0 +1,14 @@
+const RestaurantAddress = (props) => {
+
+  return (
+    <div>
+      <p>Street Address</p>
+      <p>City</p>
+      <p>State</p>
+      <p>Zip Code</p>
+    </div>
+  )
+
+}
+
+export default RestaurantAddress
