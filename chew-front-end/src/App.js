722dfb647aa2c7a53727ab13af0a0c33c56c2ba6
danishm4557 <danishm4557@gmail.com>
2022-01-27 09:37:14 -0600

    this can be ignored,changes haven't been made

--- chew-front-end/src/App.js
+++ chew-front-end/src/App.js
@@ -1,18 +1,33 @@
+import React, { Component } from 'react'
 import Nav from './components/Nav'
 import RestaurantDetailsPage from './components/RestaurantDetailsPage'
 import ReviewsBox from './components/ReviewsBox'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <img src="https://i.imgur.com/RpWwxC9.jpg" />
-      <Nav />
-      <h1 className="title">CHEW</h1>
-      <RestaurantDetailsPage />
-      <ReviewsBox />
-    </div>
-  );
+
+// let baseUrl = "http://opentable.herokuapp.com"
+// let baseUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDwOPey_5Xp_w939z6sYtwUYfTxxjNqUwY&callback=InitMap"
+
+
+class App extends Component{
+  constructor(props) {
+    super(props)
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        <img src="https://i.imgur.com/RpWwxC9.jpg" />
+        <Nav />
+        <h1 className="title">CHEW</h1>
+        <RestaurantDetailsPage />
+        <ReviewsBox />
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
---
+++ chew-front-end/src/components/Map.js
@@ -0,0 +1,13 @@
+// let map;
+// let google = "google.com"
+// const InitMap = () => {
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: {
+//       lat: -34.397,
+//       lng: 150.644
+//     },
+//     zoom: 8
+//   });
+// }
+//
+// export default InitMap
