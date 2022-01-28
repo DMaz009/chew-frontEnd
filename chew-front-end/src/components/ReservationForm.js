245394426dc0e172d6571b53d2213fe0714e4956
David Mazza <DavidMazza@Davids-MBP.attlocal.net>
2022-01-28 12:36:55 -0600

    reviewsbox update

---
+++ chew-front-end/src/components/ReservationForm.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+
+
+
+export default class ReservationForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: '',
+      reservations: [],
+      guests: '',
+      restaurant: '',
+      location: '',
+      date: '',
+      time: '',
+      availability: '',
+      reservationToBeEdited: {},
+      modalOpen: false
+
+    }
+  }
+
+
+  addReservation = (newReservation) => {
+    const copyReservations = [...this.state.reservations]
+    copyReservations.push(newReservation)
+    this.setState({
+      reservations: copyReservations
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+  }
+
+  handleChange = (event) => {
+    console.log(event.target.value)
+    this.setState({
+      name: event.target.value,
+      guests: event.target.value
+    })
+  }
+
+  render() {
+    console.log(this.state.name)
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor='name'>Name: </label>
+        <input type='text' id='name' name='name'
+          onChange={ (event) => this.handleChange(event)}
+          value={this.state.name}
+          />
+          <label htmlFor='guests'>Guests: </label>
+          <input type='number' id='guests' name='guests'
+            onChange={ (event) => this.handleChange(event)}
+            value={this.state.guests}
+            />
+
+          <input type='submit' value='Add Reservation' />
+      </form>
+    )
+
+  }
+
+}
