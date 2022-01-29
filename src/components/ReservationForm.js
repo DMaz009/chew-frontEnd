5bb678c23cea02d0acb11ae26346d042a28de187
danishm4557 <danishm4557@gmail.com>
2022-01-28 19:07:37 -0600

    test

--- src/components/ReservationForm.js
+++ src/components/ReservationForm.js
@@ -22,7 +22,7 @@ export default class ReservationForm extends Component {
   }
 
   getReservations = () => {
-    fetch(basUrl + '/chew', {
+    fetch(baseUrl + '/chew', {
       credentials: 'include'
     })
     .then(res => {
@@ -32,7 +32,7 @@ export default class ReservationForm extends Component {
         return []
       }
     }).then(data => {
-      this.setState( reservations: data)
+      this.setState({reservations: data})
     })
   }
 
